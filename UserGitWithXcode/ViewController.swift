fce4cc17d952b0a2b74a5ed8ff5da7938d08a9e2
Quang Vũ <vutran7993@gmail.com>
2019-06-12 13:09:48 +0700

    Initial Commit

---
+++ UserGitWithXcode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserGitWithXcode
+//
+//  Created by RTC-HN154 on 6/12/19.
+//  Copyright © 2019 RTC-HN154. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
